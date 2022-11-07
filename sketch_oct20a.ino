9f8eceb554b1e9e91b1e1a9f95200ae58f1bfbfc
Dion Koolhaas <dionkoolhaas@gmail.com>
2022-11-07 14:54:18 +0100

    initele code

---
+++ sketch_oct20a.ino
@@ -0,0 +1,113 @@
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#define SCHERM_BREEDTE 128  // OLED display breedte, in pixels
+#define SCHERM_HOOGTE 64    // OLED display hoogte, in pixels
+#define OLED_RESET -1
+#define SCHERM_ADRES 0x3C
+Adafruit_SSD1306 display(SCHERM_BREEDTE, SCHERM_HOOGTE, &Wire, OLED_RESET);
+WiFiClientSecure client;
+DynamicJsonDocument jsonObject(2048);
+
+const char* ssid = "JOUW-NETWERKNAAM";     // Vul hier jouw eigen netwerknaam (ssid) in
+const char* password = "JOUW-WACHTWOORD";  // Vul hier jouw eigen netwerkwachtwoord in
+const String server = "api.coindesk.com"; // Het adres van de server
+const String url = "/v1/bpi/currentprice.json"; // De url waar de informatie te vinden is
+
+
+int huidig_scherm = 0;
+int aantal_schermen = 2;
+bool button_pressed = false;
+int counter = 0;
+float huidigePrijs;
+
+void setup() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCHERM_ADRES)) {
+    // Deze eeuwigdurende lus zorgt ervoor dat de applicatie niet verder gaat bij een fout
+    for (;;)
+      ;
+  }
+
+  printPaginaNul(huidig_scherm);
+  maakVerbindingMetWifi();
+  doeApiCalls();
+}
+
+// Deze functie maakt verbinding met het netwerk
+void maakVerbindingMetWifi() {
+  WiFi.mode(WIFI_STA);                     // Type wifi verbinding instellen
+  WiFi.begin(ssid, password);              // Gebruik WiFi.begin om verbinding te maken
+  while (WiFi.status() != WL_CONNECTED) {  // Herhalen tot er verbinding is
+    display.print(".");                    // Schrijf een punt op het scherm
+    display.display();                     // Toon de teksten op het scherm
+    delay(500);                            // Wacht een halve seconde
+  }
+  client.setInsecure();
+}
+
+void clearDisplay() {
+  // Maak het scherm leeg
+  display.clearDisplay();
+  display.setTextSize(1);  // Zet het tekstformaat op 2 (2x zo groot)
+  display.setCursor(0, 0);
+  display.setTextColor(WHITE);
+}
+
+void printPaginaNul(int input) {
+  clearDisplay();
+  display.printf("Hello pagina nul: %d", input);
+  // Laat hetgeen we getekend hebben zien op het scherm
+  display.display();
+}
+
+void printPaginaEen() {
+  clearDisplay();
+  display.println("Hello pagina twee");
+  // Laat hetgeen we getekend hebben zien op het scherm
+  display.display();
+}
+
+void doeApiCalls() {
+  client.println("GET https://" + server + url + " HTTP/1.0");
+  client.println("Host: " + server);
+  client.println("Connection: close");
+  client.println();
+
+  // Wachten tot de server klaar is om de gegevens te versturen
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      break;
+    }
+  }
+  String gegevens = client.readString();  // De gegevens van de server uitlezen
+  deserializeJson(jsonObject, gegevens);  // De gegevens omzetten naar een JSON object
+  // De huidige prijs uitlezen
+  huidigePrijs = jsonObject["bpi"]["EUR"]["rate_float"].as<float>();
+  client.stop();
+}
+
+void loop() {
+
+  if (digitalRead(D3) == LOW && !button_pressed) {
+    huidig_scherm = (huidig_scherm + 1) % aantal_schermen;
+    button_pressed = true;
+  } else if (digitalRead(D3) == HIGH) {
+    button_pressed = false;
+  }
+
+  if (huidig_scherm == 1) {
+    printPaginaEen();
+  } else {
+    printPaginaNul(huidigePrijs);
+  }
+
+  if (counter > 10000) {
+    counter = 0;
+    doeApiCalls();
+  }
+  counter = counter + 1;
+}
